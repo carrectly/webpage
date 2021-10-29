44839d6e704aeee2b29c86132193fc4ac37fb361
Andre Birkus <birkusandre@gmail.com>
2021-10-28 20:28:54 -0500

    adding navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,21 @@
 import React, { FC } from 'react'
+import Header from './Header/Header'
 import styles from '../styles/Layout.module.css'
 
 type children = JSX.Element
 
 
-interface LayoutProps {
-    layout: children
-}
-
-const Layout: FC<LayoutProps> = ({children}): JSX.Element => {
+const Layout: FC = ({children}): JSX.Element => {
     return (
+        <>
+        <Header/>
         <div className={styles.container}>
             <main className={styles.main}>
+                <h1>Hello</h1>
                 {children}
             </main>
         </div>
+        </>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <meta name="Carrectly" content="car service, detailing, repair, tinting, washing"/>
+      </Head>
       <h1>
         Welcome to Next.js
       </h1>
