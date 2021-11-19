9e80a4bf71087adaa7ab5cce37a386fe512928bb
Andre Birkus <birkusandre@gmail.com>
2021-11-19 16:22:24 -0600

    adding faq page with accordion

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,15 +1,79 @@
 import React from 'react';
-import { Box, Typography, Tabs, Tab, Accordion, AccordionSummary, AccordionDetails  } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Tabs,
+  Tab,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '@mui/material';
 import Layout from '../components/Layout/Layout';
-import faqData from '../data/faq.json'
+import faqData from '../data/faq.json';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const faq = () => {
-    const [value, setValue] = React.useState('one');
+const faqSubGroupsArr = ['basics', 'services', 'payments', 'changes', 'info'];
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
 
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-      setValue(newValue);
-    };
+type faqSubGroupsTypes = 'basics' | 'services' | 'payments' | 'changes' | 'info';
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const renderAccordion = (faqDataObj: any, faqGroup: faqSubGroupsTypes): React.ReactNode => {
+  if (faqDataObj[faqGroup]) {
+    return faqDataObj[faqGroup].map((el: any, index: number) => (
+      <Accordion key={`accordiong_id_${index}`}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{el.question}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{el.answer}</Typography>
+        </AccordionDetails>
+      </Accordion>
+    ));
+  }
+  return <div />;
+};
+
+const faq = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
     <Layout>
@@ -17,34 +81,24 @@ const faq = () => {
         FAQ
       </Typography>
       <Box sx={{ width: '100%' }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          textColor="secondary"
-          indicatorColor="secondary"
-          aria-label="secondary tabs example"
-        >
-          <Tab value="one" label="Item One" />
-          <Tab value="two" label="Item Two" />
-          <Tab value="three" label="Item Three" />
-        </Tabs>
-
-        {faqData.basics && faqData.basics.map((el) => (
-                 <Accordion>
-                 <AccordionSummary
-                   expandIcon={<ExpandMoreIcon />}
-                   aria-controls="panel1a-content"
-                   id="panel1a-header"
-                 >
-                   <Typography>{el.question}</Typography>
-                 </AccordionSummary>
-                 <AccordionDetails>
-                   <Typography>
-                    {el.answer}
-                   </Typography>
-                 </AccordionDetails>
-               </Accordion>
-        ))}
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            {faqSubGroupsArr.length &&
+              faqSubGroupsArr.map((el, i) => (
+                <Tab label={el.toUpperCase()} {...a11yProps(i)} />
+              ))}
+          </Tabs>
+        </Box>
+        {faqSubGroupsArr.length &&
+          faqSubGroupsArr.map((el, i) => (
+            <TabPanel value={value} index={i}>
+              {renderAccordion(faqData, el)}
+            </TabPanel>
+          ))}
       </Box>
     </Layout>
   );
