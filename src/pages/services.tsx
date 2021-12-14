11214aa3d0bb4cf659eeb5937ecebf84420b2d88
Andre Birkus <birkusandre@gmail.com>
2021-12-13 20:25:48 -0600

    fixing accordion

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -10,22 +10,81 @@ import {
   Tab,
   Accordion,
   AccordionSummary,
+  AccordionSummaryProps,
   AccordionDetails,
 } from '@mui/material';
 import { ServicesDataType } from '../../utils/types';
+import styled from '@emotion/styled';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import StarIcon from '@mui/icons-material/Star';
+import LocalCarWashIcon from '@mui/icons-material/LocalCarWash';
+import BuildIcon from '@mui/icons-material/Build';
+import AirlineSeatReclineExtraIcon from '@mui/icons-material/AirlineSeatReclineExtra';
+import AspectRatioIcon from '@mui/icons-material/AspectRatio';
+import CarRepairIcon from '@mui/icons-material/CarRepair';
+import CarRentalIcon from '@mui/icons-material/CarRental';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+
+const iconsArr = [
+  <StarIcon />,
+  <LocalCarWashIcon />,
+  <BuildIcon />,
+  <DirectionsCarIcon />,
+  <AirlineSeatReclineExtraIcon />,
+  <AspectRatioIcon />,
+  <CarRepairIcon />,
+  <Inventory2Icon/>,
+  <CarRentalIcon />
+];
+
+const AccordionSummaryStyled = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor: '#e6e6e6',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& hover': {
+    backgroundColor: '#ddd',
+  },
+}));
+
+const styleA = {
+  backgroundColor: '#e6e6e6',
+  flexDirection: 'row',
+  '&:hover': {
+    backgroundColor: '#ddd',
+  },
+};
 
 const services = () => {
   return (
     <Layout>
+      <Grid container justifyContent='center'>
       {serviceArray.map((service, index) => (
-        <Accordion key={`accordiong_id_${index}`}>
+        <Accordion key={`accordiong_id_${index}`} sx={{width: '90vw', margin: '10px'}}>
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
+            sx={styleA}
+            expandIcon={<ExpandMoreIcon color="primary" />}
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography>{service.category}</Typography>
+            <Box>
+            {iconsArr[index]}
+            </Box>
+            <Box
+              sx={{
+                width: '100%',
+                textAlign: 'center'
+              }}
+            >
+             {service.category}
+            </Box>
           </AccordionSummary>
 
           <AccordionDetails
@@ -43,6 +102,7 @@ const services = () => {
           </AccordionDetails>
         </Accordion>
       ))}
+      </Grid>
     </Layout>
   );
 };
