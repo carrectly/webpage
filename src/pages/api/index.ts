368d5a439a79d4ba7af5150696daf970595e0692
Andre Birkus <birkusandre@gmail.com>
2022-01-17 16:35:09 -0600

    fixing ts rules

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -24,9 +24,9 @@ const cors = Cors({
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
+function runMiddleware(req: any, res: any, fn: any) {
   return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
+    fn(req, res, (result: any) => {
       if (result instanceof Error) {
         return reject(result);
       }
@@ -36,7 +36,7 @@ function runMiddleware(req, res, fn) {
   });
 }
 
-async function handler(req, res) {
+async function handler(req: any, res: any) {
   // Run the middleware
   await runMiddleware(req, res, cors);
 
