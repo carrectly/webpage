c4ddc0bcc99a5f63684186747b77594e76ce307d
Valeriia <v.braterska@gmail.com>
2022-06-09 11:46:03 +0800

    added request to write new orders to Google cloud

--- src/pages/api/sendOrder.ts
+++ src/pages/api/sendOrder.ts
@@ -1,17 +1,14 @@
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await axios.post(
-      `${process.env.CARRECTLY_ADMIN_URL}/api/newBooking`,
-      {
-        param: req.body,
-      }
-    );
+    const response = await axios.post(`${process.env.CARRECTLY_ADMIN_URL}/api/newBooking`, {
+      param: req.body,
+    });
+    await axios.post('https://carrectlyautocare.com/api/newBooking', {
+      param: req.body,
+    });
     res.status(200).json(response.statusText);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
