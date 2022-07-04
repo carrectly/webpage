6bf4923ab67b7baf5ee314590722c7076fc73fd4
Valeriia Braterska <val.braterskaya@gmail.com>
2022-07-04 09:29:03 +0300

    Update master (#32) (#33) (#36)
    * Merge remote-tracking branch 'upstream/update-services'
    
    * fixed Jenkinsfiles, endpoint for new booking
    
    * fixed Jenkinsfile for development mode
    
    * changed env
    
    * update Jenkinsfile
    
    * fixed date for order (after reloading a page)
    
    Co-authored-by: Valeriia <v.braterska@gmail.com>
    Co-authored-by: carrectly <103471695+carrectly@users.noreply.github.com>
    
    Co-authored-by: Valeriia <v.braterska@gmail.com>
    Co-authored-by: carrectly <103471695+carrectly@users.noreply.github.com>
    
    Co-authored-by: Valeriia <v.braterska@gmail.com>
    Co-authored-by: carrectly <103471695+carrectly@users.noreply.github.com>

--- src/pages/api/sendOrder.ts
+++ src/pages/api/sendOrder.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const response = await axios.post(`${process.env.CARRECTLY_ADMIN_URL}/api/newBooking`, {
       param: req.body,
     });
-    
+
     await axios.post(`${process.env.CARRECTLY_ADMIN_URL_OLD}/api/newBooking`, {
       param: req.body,
     });
